a839321ec7d3933072d0aa51d9c28e650dd6ce6b
Aretzera <107741000+AlexAn75541@users.noreply.github.com>
2025-10-23 17:51:00 +0700

    Update update.py with warning about Vietnamese translation
    Added a note about the Vietnamese translation affecting usage.

--- update.py
+++ update.py
@@ -19,6 +19,9 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+Since the Vietnamese translation is applied, therefore using this file is not recommended since it will overiding the modified Vietnamese Strings. :> 
+
 """
 
 import argparse
